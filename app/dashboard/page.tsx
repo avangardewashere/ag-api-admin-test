4abd01870f21117ebb39ddef157f028b25005317
avangardewashere <avelpanaligan@gmail.com>
2024-12-22 23:06:42 +0800

    add/dashboard-page-structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,26 @@
-
+import Card from "../ui/dashboard/card/card";
+import Charts from "../ui/dashboard/charts/charts";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transactions from "../ui/dashboard/transactions/transaction";
+import styles from "./index.module.css";
 
 const dashboard = () => {
   return (
-    <div>
-      <span>Hello DashBaord</span>
+    <div className={styles.wrapper}>
+      <div className={styles.mainSection}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+
+        <Transactions />
+        <Charts />
+      </div>
+
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
     </div>
   );
 };
