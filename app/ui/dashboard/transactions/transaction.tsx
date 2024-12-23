98b3f0fe4dafa3cf6f0c4ab84eb29f1d8ca15325
avangardewashere <avelpanaligan@gmail.com>
2024-12-23 09:39:03 +0800

    add/table-structure-in-transaction-section

--- app/ui/dashboard/transactions/transaction.tsx
+++ app/ui/dashboard/transactions/transaction.tsx
@@ -1,9 +1,81 @@
+import Image from "next/image";
 import styles from "./index.module.css";
+import defaultImg from "@/assets/images/user.png";
+const Transactions = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Image
+                src={defaultImg}
+                alt="default-no-avaatar-image"
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+              John Doe
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                Pending
+              </span>
+            </td>
+            <td>14.02.2023</td>
+            <td>Php 2,000</td>
+          </tr>
 
-const Transactions = ( ) =>{
-    return(<div className={styles.container}>
-        <span>Transactions</span>
-    </div>)
-}
+          <tr>
+            <td>
+              <Image
+                src={defaultImg}
+                alt="default-no-avaatar-image"
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+              John Doe
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>14.02.2023</td>
+            <td>Php 2,000</td>
+          </tr>
+
+          <tr>
+            <td>
+              <Image
+                src={defaultImg}
+                alt="default-no-avaatar-image"
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+              John Doe
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>14.02.2023</td>
+            <td>Php 2,000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Transactions;
