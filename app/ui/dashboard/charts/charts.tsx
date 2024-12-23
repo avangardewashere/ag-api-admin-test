58812df362dbe3823de46b6d3db95e72db44d653
avangardewashere <avelpanaligan@gmail.com>
2024-12-23 10:45:08 +0800

    done/charts-layout-design-tooltip

--- app/ui/dashboard/charts/charts.tsx
+++ app/ui/dashboard/charts/charts.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   CartesianGrid,
   Legend,
@@ -16,23 +16,23 @@ import { ClickVisitsData as data } from "@/app/types/data";
 const Charts = () => {
   return (
     <div className={styles.container}>
-      <h2>Weekly Recap</h2>
-       <ResponsiveContainer width="100%" height="100%">
+      <h2 className={styles.title}>Weekly Recap</h2>
+      <ResponsiveContainer width="100%" height="90%">
         <LineChart
           width={500}
           height={300}
           data={data}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
-          <CartesianGrid strokeDasharray={"3 3"} />
+          {/* <CartesianGrid strokeDasharray={"3 3"} /> */}
           <XAxis dataKey="name" />
           <YAxis />
-          <Tooltip />
+          <Tooltip contentStyle={{ background: "#151c2c",border:"none" }} />
           <Legend />
           <Line type="monotone" dataKey="visit" stroke="#8884d8" />
           <Line type="monotone" dataKey="click" stroke="#82ca9d" />
         </LineChart>
-      </ResponsiveContainer>  
+      </ResponsiveContainer>
     </div>
   );
 };
