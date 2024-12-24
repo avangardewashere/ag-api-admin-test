5603572230e3aa53c43738b14c0ef645ac196653
avangardewashere <avelpanaligan@gmail.com>
2024-12-24 16:10:15 +0800

    done/products-add-form-layout

---
+++ app/dashboard/products/add/page.tsx
@@ -0,0 +1,31 @@
+import styles from "./index.module.css";
+
+const AddProductsPage = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" name="title" placeholder="title" required id="" />
+        <select name="cat" id="cat">
+          <option value="general">Choose a category</option>
+          <option value="Hhme">Home</option>
+          <option value="technology">Technology</option>
+          <option value="beauty">Beauty</option>
+        </select>
+        <input type="number" name="price" placeholder="price" id="" />
+        <input type="number" name="stock" placeholder="stock" id="" />
+        <input type="text" name="color" placeholder="color" id="" />
+        <input type="text" name="size" placeholder="size" id="" />
+        <textarea
+          name="description"
+          placeholder="Item description"
+          id="desc"
+          cols={30}
+          rows={16}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductsPage;
