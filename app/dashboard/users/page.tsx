a83b5b61f7e8c7faf137c56d0157f9a16a2f042d
avangardewashere <avelpanaligan@gmail.com>
2024-12-24 13:29:43 +0800

    add/table-structure

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,13 +1,60 @@
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "./index.module.css";
-
-const UsersPage = ( ) => {
+import Link from "next/link";
+import Image from "next/image";
+import defaultImg from "@/assets/images/user.png";
+const UsersPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <Search placeholder={"Search Something..."} />
+        <Search placeholder={"Search for a user"} />
+        <Link href="dashboard/users/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
       </div>
-      <div className={styles.table}></div>
+      <div className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  className={styles.userImage}
+                  src={defaultImg}
+                  alt="no-avatar"
+                  width={40}
+                  height={40}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13.1.2022</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </div>
     </div>
   );
 };
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,22 +1,24 @@
 export interface IDashboardLayout {
-    children:React.ReactNode
+  children: React.ReactNode;
 }
 
-
 export interface MenuItem {
-    title: string;
-    path: string;
-    icon: React.ReactNode;
-  }
-  
-  export  interface MenuGroup {
-    title: string;
-    list: MenuItem[];
-  }
+  title: string;
+  path: string;
+  icon: React.ReactNode;
+}
 
+export interface MenuGroup {
+  title: string;
+  list: MenuItem[];
+}
 
-  export interface IChartData{
-    name:string,
-    visit:number,
-    click:number
-  }
+export interface IChartData {
+  name: string;
+  visit: number;
+  click: number;
+}
+
+export interface ISearch {
+  placeholder: string;
+}
