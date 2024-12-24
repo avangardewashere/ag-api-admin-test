9b606479c22c4a78719dcc9ed38a07e4041a32cb
avangardewashere <avelpanaligan@gmail.com>
2024-12-24 14:22:54 +0800

    add/products-layout-design-css-like-users-page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,9 +1,66 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import styles from "./index.module.css";
+import Link from "next/link";
+import Search from "@/app/ui/dashboard/search/search";
+import Image from "next/image";
+import defaultImg from "@/assets/images/user.png";
+const Transactions = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a product"} />
+        <Link href="dashboard/users/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  className={styles.userImage}
+                  src={defaultImg}
+                  alt="no-avatar"
+                  width={40}
+                  height={40}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>PHP 80,000</td>
+            <td>13.1.2022</td>
 
-const Products = ( ) =>{
-    return(<div className={styles.container}>
-        <span>Products</span>
-    </div>)
-}
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
-export default Products;
+export default Transactions;
--- app/dashboard/transactions/page.tsx
+++ app/dashboard/transactions/page.tsx
@@ -1,9 +1,65 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import styles from "./index.module.css";
-
-const Transactions = ( ) =>{
-    return(<div className={styles.container}>
-        <span>Transactions</span>
-    </div>)
-}
+import Link from "next/link";
+import Search from "@/app/ui/dashboard/search/search";
+import Image from "next/image";
+import defaultImg from "@/assets/images/user.png";
+const Transactions = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for a product"} />
+        <Link href="dashboard/users/add">
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  className={styles.userImage}
+                  src={defaultImg}
+                  alt="no-avatar"
+                  width={40}
+                  height={40}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>13.1.2022</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
 export default Transactions;
