008c429d08c553b662ccda08f5605a4287c12825
avangardewashere <avelpanaligan@gmail.com>
2024-12-26 07:01:21 +0800

    add/fetch-users-api-call

--- lib/api/data.ts
+++ lib/api/data.ts
@@ -1,1 +1,10 @@
- 
+import { User } from "../models/models";
+
+export const fetchUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (e) {
+    console.log("Error encountered: ", e);
+  }
+};
