9dc9a134ebbe7986bc3677b0d510470315dd20cc
avangardewashere <avelpanaligan@gmail.com>
2024-12-26 03:36:34 +0800

    add/login-css-initial-design-form-structre

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,16 @@
-const dashboard = () => {
-    return (
-      <div>
-        <span>Hello Login </span>
-      </div>
-    );
-  };
-  export default dashboard;
-  
+import styles from "./index.module.css";
+
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login </h1>
+        <input type="text" name="" placeholder="username" id="" />
+        <input type="password" name="" placeholder="passowrd" id="" />
+        <button className={styles.button}>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
