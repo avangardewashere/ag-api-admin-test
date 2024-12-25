61144ac7c99ce453243deb0238b662e5df559c74
avangardewashere <avelpanaligan@gmail.com>
2024-12-26 03:21:35 +0800

    add/user-single-page-design-css

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -5,47 +5,53 @@ const SingleUserPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
-        <Image src={defaultImg} alt="default avatar image" />
+        <div className={styles.imgContainer}>
+          <Image src={defaultImg} alt="default avatar image" />
+        </div>
+        Name Sample
       </div>
-      Name Sample
+
       <div className={styles.formContainer}>
-        <label htmlFor="username">username</label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          placeholder="username"
-        />
+        <form action="" className={styles.form}>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+          />
 
-        <label htmlFor="email">email</label>
-        <input type="email" name="email" id="email" placeholder="email" />
+          <label htmlFor="email">email</label>
+          <input type="email" name="email" id="email" placeholder="email" />
 
-        <label htmlFor="passowrd">passowrd</label>
-        <input
-          type="password"
-          name="passowrd"
-          id="passowrd"
-          placeholder="passowrd"
-        />
+          <label htmlFor="passowrd">passowrd</label>
+          <input
+            type="password"
+            name="passowrd"
+            id="passowrd"
+            placeholder="passowrd"
+          />
 
-        <label htmlFor="phone">phone</label>
-        <input type="text" name="phone" id="phone" placeholder="phone" />
+          <label htmlFor="phone">phone</label>
+          <input type="text" name="phone" id="phone" placeholder="phone" />
 
-        <label htmlFor="address">address</label>
-        <textarea name="address" id="address" placeholder="New York" />
+          <label htmlFor="address">address</label>
+          <textarea name="address" id="address" placeholder="New York" />
 
-        <label htmlFor="isAdmin">is Admin</label>
-        <select name="isAdmin" id="isAdmin">
-          <option value="true">Yes</option>
-          <option value="true">No</option>
-        </select>
+          <label htmlFor="isAdmin">is Admin</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value="true">Yes</option>
+            <option value="true">No</option>
+          </select>
 
-        <label htmlFor="isActive">is Active</label>
-        <select name="isActive" id="isActive">
-          <option value="true">Yes</option>
-          <option value="true">No</option>
-        </select>
-        
+          <label htmlFor="isActive">is Active</label>
+          <select name="isActive" id="isActive">
+            <option value="true">Yes</option>
+            <option value="true">No</option>
+          </select>
+
+          <button className={styles.button}>Update Informaiton</button>
+        </form>
       </div>
     </div>
   );
