620cd6b615659c83b0e8df958a722898233204ce
avangardewashere <avelpanaligan@gmail.com>
2024-12-26 12:47:50 +0800

    add/search-query-onsearch-component

--- app/ui/dashboard/search/search.tsx
+++ app/ui/dashboard/search/search.tsx
@@ -1,12 +1,40 @@
+"use client";
 import { ISearch } from "@/app/types";
 import styles from "./index.module.css";
 import { MdSearch } from "react-icons/md";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Search = ({ placeholder }: ISearch) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const params = new URLSearchParams(searchParams);
+
+ 
+  console.log(searchParams);
+  console.log(pathname);
+
+  const handleOnChange = (variable: string) => {
+    console.log(variable);
+
+    params.set("q", variable);
+
+    replace(`${pathname}?${params}`);
+  };
+
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="text" placeholder={placeholder} name="" id="" />
+      <input
+        type="text"
+        placeholder={placeholder}
+        name=""
+        id=""
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          handleOnChange(e.target.value)
+        }
+      />
     </div>
   );
 };
