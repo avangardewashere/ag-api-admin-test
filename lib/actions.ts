4d10919c9ee51b70244bcaabfec96408f5c98ffb
avangardewashere <avelpanaligan@gmail.com>
2024-12-27 01:49:20 +0800

    sucess/user-add-like-post-action

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,21 +1,32 @@
+import { revalidatePath } from "next/cache";
 import { User } from "./models/models";
 import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
 
-export const addUser = async (formData:FormData) => {
-    "use server"
-    const {username, email,password,phone,address,isAdmin,isActive }= Object.fromEntries(formData);  
+export const addUser = async (formData: FormData) => {
+  "use server";
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
 
-    try {
-        connectToDB();
-        const newUser =new  User({
-            username,email,password,phone,address,isAdmin,isActive,
-        })
+  try {
+    connectToDB();
+    const newUser = new User({
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
 
-        await newUser.save();
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to create new user");
+  }
 
-        
-    } catch (error) {
-        console.log(error)
-        throw new Error("failed to create new user")
-    }
-}
+  revalidatePath("/dashboard/users");
+
+  redirect("/dashboard/users");
+};
