a2a57c924016631a31d06729f2abef566ed2bec7
avangardewashere <avelpanaligan@gmail.com>
2024-12-26 14:32:30 +0800

    implement/pagination-logic

--- app/ui/dashboard/pagination/pagination.tsx
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -1,10 +1,32 @@
+'use client'
+import { usePathname, useSearchParams,useRouter } from "next/navigation";
 import styles from "./index.module.css";
+ 
 
 const Pagination = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const page = searchParams.get("page") || "1";
+
+  const params = new URLSearchParams(searchParams);
+  const ITEM_PER_PAGE = 2;
+
+  const hasPrev = ITEM_PER_PAGE * (parseInt(page) - 1);
+  const hasNext = ITEM_PER_PAGE * (parseInt(page) - 1) + ITEM_PER_PAGE * 6;
+
   return (
     <div className={styles.container}>
-      <button className={`${styles.previous}${styles.button}`}>Previous</button>
-      <button className={`${styles.next}${styles.button}`}>Next</button>
+      <button
+        disabled={!hasPrev}
+        className={`${styles.previous} ${styles.button}`}
+      >
+        Previous
+      </button>
+      <button disabled={!hasNext} className={`${styles.next} ${styles.button}`}>
+        Next
+      </button>
     </div>
   );
 };
