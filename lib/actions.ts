048d49e51e3109acf0dc5fa77fbf6443e61cbe73
avangardewashere <avelpanaligan@gmail.com>
2024-12-27 02:02:34 +0800

    security/hashed-passwords

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,18 +2,34 @@ import { revalidatePath } from "next/cache";
 import { User } from "./models/models";
 import { connectToDB } from "./utils";
 import { redirect } from "next/navigation";
-
+import bcrypt from "bcrypt";
 export const addUser = async (formData: FormData) => {
   "use server";
+//   const { username, email, password, phone, address, isAdmin, isActive } =
+//     Object.fromEntries(formData);
+
+  // Safely parse the FormData
+  const formDataEntries = Object.fromEntries(formData.entries());
   const { username, email, password, phone, address, isAdmin, isActive } =
-    Object.fromEntries(formData);
+    formDataEntries as {
+      username: string;
+      email: string;
+      password: string;
+      phone: string;
+      address: string;
+      isAdmin: string;
+      isActive: string;
+    };
 
   try {
     connectToDB();
+
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       phone,
       address,
       isAdmin,
